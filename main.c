81040c00d9eab40c6cf81dc35fd88ff72a34b385
karura195 <karla.diaz@pucp.pe>
2021-09-22 14:39:03 -0700

    Monty main

--- main.c
+++ main.c
@@ -7,28 +7,47 @@
  *
  * Return: 0
  */
+
 int main(int argc, char **argv)
 {
-	char ch;
+	stack_t *stack = NULL;
+	char *line = NULL;
+	size_t len = 0;
+	int line_size = 0;
 	char raiz[98] = "./";
 	char *file_name;
 	FILE *fp;
-	(void)argc;
+	char *token, *cmd;
 
+	lnum = 1;
+
+	if ((argc != 2) || (*(argv[1] + strlen(argv[1]) - 2) != '.'))
+	{
+		fputs("USAGE: monty file\n", stderr);
+		exit(EXIT_FAILURE);
+	}
 	file_name = _strncat(raiz, argv[1]);
-
 	fp = fopen(file_name, "r");
 	if (fp == NULL)
 	{
-		dprintf(2, "Error: Can't open file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	printf("The contents of %s file are:\n", file_name);
-
-	while ((ch = fgetc(fp)) != EOF)
-		printf("%c", ch);
-
+	while ((line_size = getline(&line, &len, fp)) != EOF)
+	{
+		line[line_size - 1] = '\0';
+		token = strtok(line, " \t\n");
+		cmd = token;
+		if (cmd != NULL && cmd[0] != '#')
+		{
+			token = strtok(NULL, " \t\n");
+			args = token;
+			if (args)
+				x = atoi(args);
+			matcher(cmd)(&stack, lnum);
+		}
+		lnum++;
+	}
 	fclose(fp);
 	return (0);
 }
