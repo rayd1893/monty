f5b6e90a21b82cd6d320fc1c4022c5d9897411c3
karura195 <karla.diaz@pucp.pe>
2021-09-23 21:34:07 -0700

    Adding divi

--- more_functions.c
+++ more_functions.c
@@ -96,6 +96,47 @@ void mul(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * div - Divides the second top element of the stack
+ * sub - Substract the second top element of the stack
+ * with the top element of the stack
+ * @stack: Linked list
+ * @line_number: Line number
+ *
+ * Return: Nothing
+ */
+
+void divi(stack_t **stack, unsigned int line_number)
+{
+	stack_t *mem;
+	stack_t *mem2;
+	int div = 0;
+
+	if (*stack && (*stack)->next)
+	{
+		if ((*stack)->n == 0)
+		{
+			dprintf(2, "L%u: division by zero\n", line_number);
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+		mem2 = (*stack)->next;
+		div = mem2->n / (*stack)->n;
+		mem = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(mem);
+		(*stack)->n = div;
+	}
+	else
+	{
+		dprintf(2, "L%u: can't div, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
  * sub - Substract the second top element of the stack
  * with the top element of the stack
  * @stack: Linked list
